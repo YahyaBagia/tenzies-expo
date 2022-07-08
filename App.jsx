97dc11e15c33fecf0fb8b3c7d43cd41754b04b86
Yahya Bagia <yahyabagia@gmail.com>
2022-07-09 00:51:26 +0530

    Game Sounds added

--- App.jsx
+++ App.jsx
@@ -1,8 +1,13 @@
+import { useEffect, useState } from "react";
+import { View, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Provider as PaperProvider, DefaultTheme } from "react-native-paper";
+import * as SplashScreen from "expo-splash-screen";
 
 import Main from "./src/Main";
-import { Colors } from "./src/common/Const";
+import Utils from "./src/common/Utils";
+import { Colors, Images } from "./src/common/Const";
+import { LoadLocallyCachedState } from "./src/common/GlobalState";
 
 const theme = {
   ...DefaultTheme,
@@ -13,11 +18,36 @@ const theme = {
   },
 };
 
-const App = () => (
-  <PaperProvider theme={theme}>
-    <Main />
-    <StatusBar style="auto" />
-  </PaperProvider>
-);
+const App = () => {
+  const [isGlobalStateLoaded, setIsGlobalStateLoaded] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      await SplashScreen.preventAutoHideAsync();
+      await LoadLocallyCachedState();
+      await Utils.Sleep(2);
+      setIsGlobalStateLoaded(true)
+      await SplashScreen.hideAsync();
+    })()
+  }, [])
+
+  if (!isGlobalStateLoaded)
+    return (
+      <View style={{ flex: 1, backgroundColor: Colors.Primary }}>
+        <Image
+          source={Images.Splash}
+          style={{ height: "100%", width: "100%" }}
+          resizeMode={"contain"}
+        />
+      </View>
+    );
+
+  return (
+    <PaperProvider theme={theme}>
+      <Main />
+      <StatusBar style="auto" />
+    </PaperProvider>
+  )
+};
 
 export default App;
