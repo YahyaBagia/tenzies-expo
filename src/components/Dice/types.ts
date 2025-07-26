dc3e75726c1d3a754fadb4636a7a40722624b3fd
Yahya Bagia <yahya.bagia@hotmail.com>
2025-07-26 19:15:16 +0530

    MInor refactoring

--- src/components/Dice/types.ts
+++ src/components/Dice/types.ts
@@ -4,6 +4,12 @@ export type DiceType = (typeof DiceTypes)[number];
 
 export type DiceNumber = "1" | "2" | "3" | "4" | "5" | "6";
 
+export interface IDice {
+  title: DiceNumber;
+  isSelected: boolean;
+  id: string;
+}
+
 export interface DotPosition {
   top?: number;
   bottom?: number;
